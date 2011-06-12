87aaf38b8444708f479b479e53a26b13b2bf7fc9
Amit Dhakad <amitdha@thoughtworks.com>
2011-06-12 11:07:22 +0530

    #31 - Amit : Welcome controller to render home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 NewDhakadsCom::Application.routes.draw do
   resources :sessions, :only => [:new, :create]
-
   get '/sessions/login', :to => 'sessions#new', :as => 'login'
+
+  root :to => 'welcome#home'
 end
